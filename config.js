49305ae0b31129bab8a8cdf66626e0799a411086
rule1001 <hello@rule1001.com>
2017-06-23 12:53:45 +0100

    Refactor test.seed and api.spec

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
     },
     PORT: {
         test: 3090,
-        dev: 6000
+        dev: 3000
     }
 };
--- spec/api.spec.js
+++ spec/api.spec.js
@@ -3,23 +3,31 @@ const { expect } = require('chai');
 const request = require('supertest');
 const server = require('../server');
 const saveTestData = require('../seed/test.seed');
-const config = require('../config');
-const db = config.DB[process.env.NODE_ENV] || process.env.DB;
+// const config = require('../config');
+// const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 const mongoose = require('mongoose');
 
 describe('API', function () {
-    let usefulIds;
+    this.timeout(10000)
+    let usefulData;
     before((done) => {
         mongoose.connection.dropDatabase()
-            .then(() => saveTestData(db, function (err, data) {
-                usefulIds = data;
+            .then(saveTestData)
+            .then(data => {
+                usefulData = data;
+                console.log(`Useful data: ${Object.keys(usefulData)}`);
                 done();
-            }));
+            })
+            .catch((err) => {
+                console.log(err);
+                done(err);
+            });
     });
+
     describe('GET /', function () {
         it('responds with status code 200', function (done) {
             request(server)
-                .get('/')
+                .get('/api')
                 .end((err, res) => {
                     if (err) done(err);
                     else {
@@ -55,7 +63,7 @@ describe('API', function () {
                     if (err) res.status(500);
                     else {
                         expect(res.status).to.equal(200);
-                        expect(res.body.length).to.equal(1);
+                        expect(res.body.articles.length).to.equal(1);
                         done();
                     }
                 });
@@ -75,7 +83,7 @@ describe('API', function () {
                 });
         });
     });
-    describe('GET /api/articles/:article_id/comments', function () {
+    xdescribe('GET /api/articles/:article_id/comments', function () {
         it('returns a list of comments from a single article', function (done) {
             //get the topic with the title of football and find its id use that is to construct the string
             // use model from db then do request
