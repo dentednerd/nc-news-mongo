4f32aee5003a19bf8321da6123770cf03be29af9
Joey Imlay <jo.imlay@gmail.com>
2020-03-09 16:11:35 +0000

    changed dev database to mLab deployment

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   DB: {
     test: 'mongodb://localhost/northcoders-news-api-test',
-    dev: 'mongodb://ncnews-mongo/northcoders-news-api'
+    dev: 'mongodb://dentednerd:ForTheHorde678@ds227045.mlab.com:27045/ncnews-mongo'
   },
   PORT: {
     test: 3090,
